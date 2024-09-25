ca1ba67201a309a603d95f42772430ce2e2bd3b3
Nishant Vyas <nishantvyas@gmail.com>
2024-09-25 14:30:48 -0700

    commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter(),
-		alias: {
-			$lib: path.resolve(__dirname, './src/lib'),
-			$components: path.resolve(__dirname, './src/lib/components'),
-			$utils: path.resolve(__dirname, './src/lib/utils'),
-			$stores: path.resolve(__dirname, './src/lib/stores'),
-		}
-	}
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter()
+  }
 };
 
 export default config;
